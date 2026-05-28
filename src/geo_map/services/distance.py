178a995835b73f79579c1b969ce45167e590f3ee
hay20228048 <hay20228048@std.psut.edu.jo>
2026-05-29 01:43:12 +0300

    Remove pycache from repository tracking

--- src/geo_map/services/distance.py
+++ src/geo_map/services/distance.py
@@ -3,12 +3,13 @@ from src.utils.config import GOOGLE_MAPS_API_KEY
 from src.geo_map.services.buses import PopulateBuses
 from src.geo_map.services.stops import PopulateStops
 
+
 class DistanceMatrix:
     def __init__(self):
         self.d = []
         self.buses_service = PopulateBuses()
         self.stops_services = PopulateStops()
-    
+
     async def distance_metrix(self):
         # """
         # Compute ETA from each bus to each stop using Google Distance Matrix API.
@@ -18,14 +19,13 @@ class DistanceMatrix:
         if not buses or not stops:
             return {"error": "No buses or stops defined"}
 
-
         # Prepare origins (buses) and destinations (stops)
-        origins = "|".join(f"{bus['location']['lat']},{bus['location']['lng']}" for bus in buses)
-        destinations ="|".join(f"{stop["location"]['lat']},{stop["location"]['lng']}" for stop in stops)
-
-        print("\n ++++++++++++++++++++++++++++", destinations)
-
-
+        origins = "|".join(
+            f"{bus['location']['lat']},{bus['location']['lng']}" for bus in buses
+        )
+        destinations = "|".join(
+            f"{stop['location']['lat']},{stop['location']['lng']}" for stop in stops
+        )
 
         if not destinations:
             return {"error": "Stops have no geocoded location yet"}
@@ -49,18 +49,18 @@ class DistanceMatrix:
             for j, stop in enumerate(stops):
                 element = data["rows"][i]["elements"][j]
                 if element.get("status") == "OK":
-                    bus_result["etas"].append({
-                        "stop_id": stop["id"],
-                        "distance_meters": element["distance"]["value"],
-                        "duration_seconds": element["duration"]["value"],
-                        "duration_text": element["duration"]["text"]
-                    })
+                    bus_result["etas"].append(
+                        {
+                            "stop_id": stop["id"],
+                            "distance_meters": element["distance"]["value"],
+                            "duration_seconds": element["duration"]["value"],
+                            "duration_text": element["duration"]["text"],
+                        }
+                    )
                 else:
-                    bus_result["etas"].append({
-                        "stop_id": stop["id"],
-                        "error": element.get("status")
-                    })
+                    bus_result["etas"].append(
+                        {"stop_id": stop["id"], "error": element.get("status")}
+                    )
             results.append(bus_result)
-            print("--------"*20)
-        print(results)
+
         return results
